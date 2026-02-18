5ff442ceb64afbdb37aaf68e18fa5fa4045313d1
Alexander <alexanderbatyuta@yandex.ru>
2026-02-18 11:51:28 +0700

    ver 8.3

--- Frontend/vivarium/src/utils/avatarMap.js
+++ Frontend/vivarium/src/utils/avatarMap.js
@@ -6,6 +6,7 @@ import redSlime from "../img/icons/red_slime.svg";
 import lightBlueSlime from "../img/icons/light_blue_slime.svg";
 
 export const avatarMap = {
+  // По имени файла
   "yellow_slime.svg": yellowSlime,
   "blue_slime.svg": blueSlime,
   "purple_slime.svg": purpleSlime,
@@ -14,11 +15,11 @@ export const avatarMap = {
 };
 
 export const avatarOptions = [
-  { id: 1, file: "yellow_slime.svg", image: yellowSlime, color: "#FFD700", name: "Желтый слайм" },
-  { id: 2, file: "blue_slime.svg", image: blueSlime, color: "#4169E1", name: "Синий слайм" },
-  { id: 3, file: "purple_slime.svg", image: purpleSlime, color: "#800080", name: "Фиолетовый слайм" },
-  { id: 4, file: "red_slime.svg", image: redSlime, color: "#DC143C", name: "Красный слайм" },
-  { id: 5, file: "light_blue_slime.svg", image: lightBlueSlime, color: "#87CEEB", name: "Голубой слайм" },
+  { id: 1, file: "yellow_slime.svg", image: yellowSlime, color: "#5d6939", name: "Желтый слайм" },
+  { id: 2, file: "blue_slime.svg", image: blueSlime, color: "#5d6939", name: "Синий слайм" },
+  { id: 3, file: "purple_slime.svg", image: purpleSlime, color: "#5d6939", name: "Фиолетовый слайм" },
+  { id: 4, file: "red_slime.svg", image: redSlime, color: "#5d6939", name: "Красный слайм" },
+  { id: 5, file: "light_blue_slime.svg", image: lightBlueSlime, color: "#5d6939", name: "Голубой слайм" },
 ];
 
 // Функция для получения аватарки по имени файла
